057367767c16eb98e914c7e109627dd3da283b81
NicolasFSC <barto2333@hotmail.com>
2025-04-20 21:59:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,34 +6,33 @@ $(document).ready(function(){
   });
 
   $(window).on('scroll load',function(){
+
     $('#menu').removeClass('fa-times');
     $('header').removeClass('toggle');
 
     if($(window).scrollTop() > 0){
       $('.top').show();
-    } else {
+    }else{
       $('.top').hide();
     }
+
   });
 
-  // smooth scrolling
+  // smooth scrolling 
+
   $('a[href*="#"]').on('click',function(e){
+
     e.preventDefault();
+
     $('html, body').animate({
-      scrollTop : $($(this).attr('href')).offset().top
-    }, 500, 'linear');
-  });
 
-  // Modo claro/oscuro
-  $('#toggle-mode').click(function() {
-    $('body').toggleClass('light-mode');
+      scrollTop : $($(this).attr('href')).offset().top,
 
-    // Cambia el texto del botón según el modo
-    if ($('body').hasClass('light-mode')) {
-      $('#toggle-mode').text('Modo Oscuro');
-    } else {
-      $('#toggle-mode').text('Modo Claro');
-    }
+    },
+      500, 
+      'linear'
+    );
+
   });
 
 });
